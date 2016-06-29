6619d5811b087827e6635ff53f8d25b7cd601161
Mike Dang <mike@packstack.co>
2016-06-28 22:52:05 -0500

    Moved to the js folder

--- solution.js
+++
@@ -1,9 +0,0 @@
-var sequence = [0, 1];
-var stop = 100;
-
-// Skip the first two positions since they're already defined
-for (var i=2; i<=stop; i++) {
-  sequence[i] = sequence[i - 2] + sequence[i - 1];
-}
-
-console.log(sequence);
