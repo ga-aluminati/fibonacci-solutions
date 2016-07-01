5ae0475048a41e5f0013dfd8897535f434cbef4d
Mike Dang <mike@packstack.co>
2016-07-01 12:51:19 -0500

    Update 1.php

--- php/1.php
+++ php/1.php
@@ -4,7 +4,7 @@ $sequence = [0, 1];
 $iterations = 100;
 
 for ($i=2; $i<=$iterations; $i++) {
-	$sequence[] = $sequence[$i-1] + $sequence[$i-2];
+  $sequence[] = $sequence[$i-1] + $sequence[$i-2];
 }
 
 print_r(join(',', $sequence));
